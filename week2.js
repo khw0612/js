3884310b70993dea136c3feaac37079583cef0ae
khw0612 <[dhdh588@gmail.com]>
2023-05-25 14:02:01 +0900

    assignment_calculator

---
+++ week2.js
@@ -0,0 +1,57 @@
+const readline=require('readline-sync');
+const input=readline.question;
+
+while(true)
+{
+    let running = input("Enter expression if you want to run program or Enter 'q' key if you want to exit program: ");
+    let expression = running.split(" ");
+
+    let calculator = 
+    {
+        num1: parseInt(expression[0]),
+        num2: parseInt(expression[2]),
+        operator: expression[1],
+        calculation: function()
+        {
+        
+        if(this.operator === '+')
+        console.log(running + " = " + (this.num1 + this.num2))
+
+        else if(this.operator === '-')
+        console.log(running + " = " + (this.num1 - this.num2))
+
+        else if(this.operator === '*')
+        console.log(running + " = " + (this.num1 * this.num2))
+        
+        else if(this.operator === '/')
+        console.log(running + " = " + (this.num1 / this.num2))
+        
+        else if(this.operator === '&')
+        console.log(running + " = " + (this.num1 & this.num2))
+        
+        else if(this.operator === '|')
+        console.log(running + " = " + (this.num1 | this.num2))
+        
+        else if(this.operator === '^')
+        console.log(running + " = " + (this.num1 ^ this.num2))
+
+        else if(this.operator === '>>')
+        console.log(running + " = " + (this.num1 >> this.num2))
+
+        else if(this.operator === '<<')
+        console.log(running + " = " + (this.num1 << this.num2))
+
+        else if(this.operator === '>>>')
+        console.log(running + " = " + (this.num1 >>> this.num2))
+
+        else
+        console.log("Invalid operator")
+        }   
+    }
+
+    if(running ==='q'){
+    console.log("program exit");
+    break;}
+    
+    calculator.calculation();
+}
